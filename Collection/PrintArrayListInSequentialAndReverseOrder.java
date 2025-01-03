032efb591b6d709717db6616207118aed6ef1143
gitTridip <tridippramanik2003@gmail.com>
2025-01-04 01:33:57 +0530

    03/01/2025

---
+++ Collection/PrintArrayListInSequentialAndReverseOrder.java
@@ -0,0 +1,20 @@
+package Programming.Collection;
+
+import java.util.ArrayList;
+
+public class PrintArrayListInSequentialAndReverseOrder {
+	public static void main(String[] args) {
+		ArrayList<Object> al=new ArrayList<Object>();
+		al.add("Mohan");
+		al.add(10);
+		al.add(5);
+		
+		for(int i=0;i<al.size();i++) {
+			System.out.println(al.get(i));
+		}
+		System.out.println("------------------------");
+		for(int i=al.size()-1;i>=0;i--) {
+			System.out.println(al.get(i));
+		}
+	}
+}
---
+++ Collection/PrintOnlySpecifiedType.java
@@ -0,0 +1,41 @@
+package Programming.Collection;
+
+import java.util.ArrayList;
+
+public class PrintOnlySpecifiedType {
+	public static void main(String[] args) {
+		ArrayList<Object> al=new ArrayList<Object>();
+		al.add(5);
+		al.add('c');
+		al.add('h');
+		al.add("Aandu");
+		al.add("Gandu");
+		al.add("Qspiders");
+		al.add("akfvjs");
+		System.out.println(al);
+		System.out.println();
+		System.out.println("Only Integer");
+		for(Object i:al) {
+			if(i instanceof Integer)
+				System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Only Character");
+		for(Object i:al) {
+			if(i instanceof Character)
+				System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Strings start with A/a");
+		for(Object i:al) {
+			if(i instanceof String && (((String) i).charAt(0)=='A' || ((String) i).charAt(0)=='a'))
+				System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Strings end with S/s");
+		for(Object i:al) {
+			if(i instanceof String && (((String) i).charAt(((String) i).length()-1)=='S' || ((String) i).charAt(((String) i).length()-1)=='s'))
+				System.out.println(i);
+		}
+	}
+}
